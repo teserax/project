d782d3fe1e6ce1a3c16809795542bf788d6f7f68
Steel <steeles80@mail.ru>
2020-09-30 14:50:31 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const answer = [];
+answer [0] = prompt ('how are you','');
+answer [1] = prompt ('how are you','');
+answer [2] = prompt  ('how are you','');
+
+console.log(typeof(answer));
+
+const users = `WERT`; 
+const name = `steel`;
+alert(`привет,${users},${name}`);
